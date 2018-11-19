525de14384dfa62d927a73c51c06637e00c092fb
HeikkiHietala <45161525+HeikkiHietala@users.noreply.github.com>
2018-11-19 09:49:18 +0200

    Add files via upload

---
+++ lcd-ultrasound-dist.py
@@ -0,0 +1,24 @@
+from time import sleep_ms, ticks_ms
+from machine import I2C, Pin, DAC, PWM
+import esp8266_i2c_lcd 
+from hcsr04 import HCSR04
+
+#sensor pins are set here
+sensor = HCSR04(trigger_pin=16, echo_pin=2)
+#these set up the display pins
+i2c = I2C(scl=Pin(21), sda=Pin(22), freq=400000)
+#at the end, the 4 is for rows, the 20 for columns, so adjust to suit your display
+lcd = esp8266_i2c_lcd.I2cLcd(i2c, 0x27, 4, 20)
+
+def main():
+
+  while True:
+    distance = sensor.distance_cm() #get the distance
+    message = "Distance: " + str(int(distance)) + " cm" #prepare LCD message
+    lcd.putstr(message) # show it
+    time.sleep_ms(500) #wait half a second before measuring again
+    lcd.clear() #clear the LCD, otherwise you get trailing characters
+  
+main()
+
+
